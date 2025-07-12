6b0218e639f6042f943c118fac9a29409d471c01
KiranTotager <tskiran5114@gmail.com>
2025-07-12 11:10:06 +0530

    project set up

---
+++ bookbart/src/test/java/com/kiran/bookbart/BookbartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiran.bookbart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookbartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
