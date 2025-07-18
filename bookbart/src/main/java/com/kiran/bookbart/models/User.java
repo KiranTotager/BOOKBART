431fe38be190e177ed3ef9ad75207f6b56a2ccf8
KiranTotager <tskiran5114@gmail.com>
2025-07-18 12:22:39 +0530

    user model created

---
+++ bookbart/src/main/java/com/kiran/bookbart/models/User.java
@@ -0,0 +1,26 @@
+package com.kiran.bookbart.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false,unique = true)
+    private String phone;
+}
